237ff8e6bd4b6e45913814eea438f9d794aaecf4
Zheng1013 <love24175160@gmail.com>
2023-12-18 17:07:05 +0800

    12/18/17:06

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:a12345@mysql:3306/test2"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:a12345@mysql:3306/test2"
+SQLALCHEMY_DATABASE_URL = "postgresql://kong:kong@kong-database:5432/api"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
